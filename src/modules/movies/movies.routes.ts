ff7ca85a7a2c05b519c9b54a03d8b6c5867953bc
Abdulal-noman <mdroman45678910@gamil.com>
2025-08-14 02:06:01 +0600

    add reviews sucessfully

--- src/modules/movies/movies.routes.ts
+++ src/modules/movies/movies.routes.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/", MovieController.createMovie);
 router.get("/", MovieController.getAllMovies);
 router.get("/:slug", MovieController.getMovieBySlug);
-router.post("/:slug/reviews", ReviewController.addReview);
+router.post("/:slug/review", ReviewController.addReview);
 // router.get("/:slug/reviews", ReviewController.getAllReviews);
 // router.put("/:slug/review", ReviewController.updateReview);
 // router.delete("/:slug/review", ReviewController.deleteReview);
